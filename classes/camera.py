2ce0d9f88b9a59cb0bbd56f03d8c06173a20324d
Mitch LeBlanc <supermitch@gmail.com>
2015-02-28 07:57:38 -0800

    Minor cleanup

--- classes/camera.py
+++ classes/camera.py
@@ -11,22 +11,19 @@ class Camera(object):
         self.half_w = int(screen_w / 2)
         self.half_h = int(screen_h / 2)
 
-    def apply(self, target_rect):
-        return target_rect.move(self.state.topleft)
-
     def update(self, target_rect):
+        """ Update our position according to the player's position. """
         l, t, _, _ = target_rect
         _, _, w, h = self.state
-        # center player
-        l, t, _, _ = -l + self.half_w, -t + self.half_h, w, h
+        l, t, _, _ = -l + self.half_w, -t + self.half_h, w, h  # Center player
 
-        # stop scrolling at the left edge
-        l = min(0, l)
-        # stop scrolling at the right edge
-        l = max(-(self.state.width - self.screen_w), l)
-        # stop scrolling at the bottom
-        t = max(-(self.state.height - self.screen_h), t)
-        # stop scrolling at the top
-        t = min(0, t)
+        l = min(0, l)  # stop scrolling at the left edge
+        l = max(-(self.state.width - self.screen_w), l)  # Stop at right
+        t = max(-(self.state.height - self.screen_h), t)  # Stop at bottom
+        t = min(0, t)  # Stop at top
         self.state = Rect(l, t, w, h)
+
+    def apply(self, target_rect):
+        """ Return a rectangle centered according to our location. """
+        return target_rect.move(self.state.topleft)
 
