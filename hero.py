2383510041e701d339c0693f7b9629ca183188b5
Mitch LeBlanc <supermitch@gmail.com>
2015-02-27 23:42:02 -0800

    Add pygame.quit()

--- hero.py
+++ hero.py
@@ -4,10 +4,13 @@ from classes.game import Game
 
 def main():
     pygame.init()
+
     game = Game()
     game.main_loop()
+
+    pygame.quit()
     print('Goodbye.')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
 
